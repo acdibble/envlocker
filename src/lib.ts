fe347a2a97faecf1f586afef88021761505d344d
Andrew Dibble <andrew@dibble.codes>
2023-06-19 21:43:26 +0200

    add docs

--- src/lib.ts
+++ src/lib.ts
@@ -10,15 +10,40 @@ type OpEnv = {
   account?: string;
 };
 
-type InjectOptions = {
+type ConfigOptions = {
+  /** the target into which the new env variables should be injected, default is `process.env` */
   target?: Record<string, string>;
+  /** the name of the environment to load, default is `process.env['ENVLOCKER_ENV_NAME'] ?? 'development'` */
   envName?: string;
 };
 
+/**
+ * Inject the environment variables from 1password into the current process
+ *
+ * With `import`:
+ * @example
+ * ```ts
+ * import 'envlocker/config';
+ * ```
+ *
+ * @example
+ * ```ts
+ * import { config } from 'envlocker';
+ * config({ envName: 'staging' });
+ * ```
+ *
+ * or with `require`:
+ * @example
+ * ```ts
+ * require('envlocker').config();
+ * ```
+ *
+ * @param options
+ */
 export const config = ({
   target = process.env as Record<string, string>,
   envName = process.env['ENVLOCKER_ENV_NAME'] ?? 'development',
-}: InjectOptions = {}): void => {
+}: ConfigOptions = {}): void => {
   try {
     const opEnv = JSON.parse(
       fs.readFileSync(path.join(process.cwd(), '.envlockerrc'), 'utf8'),
@@ -49,25 +74,46 @@ export const config = ({
 };
 
 type CreateItemFromEnvFileOptions = {
+  /** the path to the .env file */
   filePath: string;
+  /** the title for the item in 1password */
   title: string;
+  /** the vault to house th enew item */
   vault: string;
+  /** an optional account for 1password */
   account?: string;
+  /** an optional category for 1password, defaults to `"Server"` */
   category?: op.InputCategory;
 };
 
+/**
+ * Insert an existing .env file into 1password
+ * @returns the newly created item
+ *
+ * @example
+ * ```ts
+ * import { createItemFromEnvFile } from 'envlocker';
+ *
+ * await createItemFromEnvFile({
+ *   filePath: path.join(process.cwd(), '.env'),
+ *   title: 'My App',
+ *   vault: 'My Vault',
+ * });
+ * ```
+ *
+ */
 export const createItemFromEnvFile = async ({
   filePath,
   title,
   vault,
   account,
   category = 'Server',
-}: CreateItemFromEnvFileOptions) => {
+}: CreateItemFromEnvFileOptions): Promise<op.Item> => {
   const env = parse(await fs.promises.readFile(filePath, 'utf8'));
 
   const fieldAssignments = Object.entries(env)
     .filter(([key, value]) => Boolean(key && value && /^[\dA-Z_]+$/.test(key)))
     .map(([key, value]) => [key, 'concealed', value] as op.FieldAssignment);
 
-  op.item.create(fieldAssignments, { vault, title, account, category });
+  return op.item.create(fieldAssignments, { vault, title, account, category });
 };
